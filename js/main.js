5c438467a1def14cc687799ff50d10663ead0a92
MMR Auerbach <m.auerbach@rocmondriaan.nl>
2021-04-20 17:16:03 +0200

    added vert and diag check

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 const board = document.querySelector('.tic-tac-toe');
 const popupElement = document.querySelector('.popup-wrapper');
 const popupElementSection = document.querySelector('.popup');
-const boardVH = 2;
+const boardVH = 5;
+const xSound = new Audio('lib/tic.mp3');
+const ySound = new Audio('lib/tac.mp3');
+const weSound = new Audio('lib/we.mp3');
+
 let fields;
 let winX = '';
 let winO = '';
@@ -17,17 +21,32 @@ if (popupElementSection) {
 function addItem(field) {
     if (field.textContent.trim() == '') {
         field.textContent = curElement;
+        if (curElement == 'X') {
+            ySound.play();
+        } else {
+            xSound.play();
+        }
+
         curElement = (curElement == 'X') ? 'O' : 'X';
         checkWinner();
     }
 }
 
 function checkWinner() {
-    let vertSeries = [];
+    const vertSeries = [];
+    let lrDiag = '';
+    let lrNext = 0;
+    let lrCountNext = true;
+
+    let rlNext = (boardVH-1);
+    let rlCountNext = true;
+    let rlDiag = '';
+
     let series = '';
     let haveWinner = false;
     let filledFields = 0;
 
+    //Loop through fields and check horizontal winner
     fields.forEach(function(field, idx) {
         const fieldSymbol = field.textContent;
         if (fieldSymbol === 'X' || fieldSymbol === 'O')
@@ -42,23 +61,57 @@ function checkWinner() {
         vertSeries[(idx%boardVH)] += fieldSymbol;
         series += fieldSymbol;
 
+        if (rlNext === (idx%boardVH) && (idx < ((boardVH*boardVH) -1))) {
+            rlNext--;
+            if (rlNext < 0)
+                rlNext = boardVH-1;
+
+            rlDiag += fieldSymbol;
+        }
+
+        if (lrNext === (idx%boardVH)) {
+            if (lrCountNext) {
+                lrDiag += fieldSymbol;
+                lrNext++;
+                lrCountNext = false;
+            } else {
+                lrCountNext = true;
+            }
+        }
+
         if (series === winX || series === winO) {
-            popupElement.classList.toggle('hidden');
+            showPopup('Winner winner', 'Chicken Dinner', 'warning');
             haveWinner = true;
         }
     });
 
+    //Check for vertical winners
     if (!haveWinner) {
         vertSeries.forEach(function(serie) {
             if (serie === winX || serie === winO) {
-                popupElement.classList.toggle('hidden');
+                showPopup('Winner winner', 'Chicken Dinner', 'warning');
                 haveWinner = true;
             }
         });
     }
 
+    //Check for diagonal winners
+    if (!haveWinner) {
+        if (lrDiag == winX || lrDiag == winO) {
+            showPopup('Winner winner', 'Chicken Dinner', 'warning');
+                haveWinner = true;
+        }
+    }
+
+    if (!haveWinner) {
+        if (rlDiag == winX || rlDiag == winO) {
+            showPopup('Winner winner', 'Chicken Dinner', 'warning');
+                haveWinner = true;
+        }
+    }
+
     if ((filledFields === boardVH*boardVH) && !haveWinner) {
-        popupElement.classList.toggle('hidden');
+        showPopup('Loser loser', 'Chicken Foser', 'danger');
     }
 }
 
@@ -66,6 +119,9 @@ function resetGame() {
     fields.forEach(function(field) {
         field.innerHTML = '&nbsp;';
     });
+
+    weSound.pause();
+    weSound.currentTime = 0;
 }
 
 function initBoard(rowCol = 2) {
@@ -90,11 +146,17 @@ function initBoard(rowCol = 2) {
     });
 }
 
-function showPopup(title, message, colour = yellow) {
-    popupElement.querySelector('.')
+function showPopup(title, message, alert = warning) {
+    popupElement.querySelector('.title').textContent = title;
+    popupElement.querySelector('.text').textContent = message;
+    popupElement.querySelector('.popup').classList.remove('alert-danger','alert-warning');
+    popupElement.querySelector('.popup').classList.add(`alert-${alert}`);
+
+    if (alert === 'warning') {
+        weSound.play();
+    }
+
     popupElement.classList.remove('hidden');
 }
 
-
 initBoard(boardVH);
-
